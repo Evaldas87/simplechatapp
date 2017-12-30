6c1d663b677070c242750d8a76bb09fb82f50452
Evaldas <maybeat@hotmail.com>
2017-12-30 20:43:59 +0200

    users list added, other improvements

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,5 @@
 // make connection
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect();
 
 // Query DOM
 var message = document.getElementById('message');
@@ -10,6 +10,7 @@ var feedback = document.getElementById('feedback');
 var enternamebtn = document.getElementById('enternamebtn');
 var entername = document.getElementById('entername');
 var simplechatapp = document.getElementById('simplechatapp');
+var users = document.getElementById('users');
 
 // emit evets
 
@@ -19,6 +20,7 @@ btn.addEventListener('click', function() {
 		handle: handle.value,
 		});
 	window.scrollTo(0, document.body.scrollHeight);
+	message.value = "";
 });
 
 message.addEventListener('keypress', function() {
@@ -26,23 +28,31 @@ message.addEventListener('keypress', function() {
 })
 
 
-
 // listen for events
 
 socket.on('chat', function(data){
 	feedback.innerHTML = "";
-	message.value = "";
-	output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+	output.innerHTML += '<p><strong>' + data.handle + '> </strong>' + data.message + '</p>';
 })
 
 socket.on('typing', function(data){
 	feedback.innerHTML = "<p><em>" + data + " is typing...</em></p>";
 })
 
+socket.on('users', function(data){
+	var names = '';
+	for (var i = 0; i < data.length; i++) {
+		names += "<li>" + data[i] + "</li>";
+	}
+	users.innerHTML = names;
+})
+
+
 // Design events
 
 enternamebtn.addEventListener('click', function() {
 	entername.style = "display: none";
 	simplechatapp.style = "display: inline";
-
+	socket.emit('new user', handle.value, function(data){
+	});
 })
